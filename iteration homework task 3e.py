86bce9497a29826861a714bfd37f044e912fc273
Laptop <Laptop@Laptop-PC>
2014-10-14 21:10:05 +0100

    .

---
+++ iteration homework task 3e.py
@@ -0,0 +1,19 @@
+# mateusz sacha
+#14-10-2014
+# iteration homework 3e
+number = int(input("Enter a number to get its multiplication table:"))
+x1 = number*1
+x2 = number*2
+x3 = number*3
+x4 = number*4
+x5 = number*5
+x6 = number*6
+x7 = number*7
+x8 = number*8
+x9 = number*9
+x10 = number*10
+x11 = number*11
+x12 = number*12
+print("{0}*1= {1}, {0}*2= {2}, {0}*3= {3}, {0}*4= {4}, {0}*5= {5}, {0}*6= {6}, {0}*7= {7}, {0}*8= {8}, {0}*9= {9}, {0}*10= {10}, {0}*11= {11}, {0}*12= {12}".format(number,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12))
+
+    
---
+++ iteration homework task 6.1.py
@@ -0,0 +1,7 @@
+#Mateusz Sacha
+#13-10-2014
+#iteration homework task 6.1
+word = input("Enter a word:")
+number_of_times_to_print = int(input("Enter the number of times you want your text to be printed:"))
+for count in range(number_of_times_to_print):
+    print(word)
