04903306ede895fe89758d98ea2ef37d1412533c
Mateusz Sacha <45197@longroad.ac.uk>
2014-10-22 15:16:05 +0100

    stater task 4

---
+++ iteration starter task 5.py
@@ -0,0 +1,8 @@
+# Mateusz Sacha
+# 22-10-2014
+# iteration starter task 5
+answer = True
+for count in range(2,-1):
+    remainder = number + count
+    if remainder = 0:
+        answer = False
--- task 1 improving a piece of code.py
+++ task 1 improving a piece of code.py
@@ -1,5 +1,4 @@
 total = 0
 for count in range (1,9):
     total = total + int(input("Enter a number:"))
-    
 print('The total is {0}'.format(total))
