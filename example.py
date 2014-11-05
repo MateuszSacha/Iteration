bce044054da0490a3fc67e10b6aa228c9f280678
Mateusz Sacha <45197@longroad.ac.uk>
2014-11-05 14:57:30 +0000

    example doesnt work

---
+++ example.py
@@ -0,0 +1,10 @@
+number = int(input("Please enter a number: "))
+high = max(number)
+low = min(number)
+while number == -1:
+    number = int(input("Please enter a number: "))
+print ("The highest number entered was {0}".format(high))
+print ("The lowest number entered was {0}".format(low))
+
+
+
