54df509bd15195af903dfae15a6ac6234df663c1
Mateusz Sacha <45197@longroad.ac.uk>
2014-11-11 13:54:49 +0000

    guess the number game not finished

--- development exercise 1.py
+++ development exercise 1.py
@@ -1,6 +1,14 @@
 # Mateusz Sacha
 # 05-11-2014
 # Iteration Development exercise 1
-number = int(input("Enter a number:"))
+number = 0
+largest = 0
 while not number == -1:
-    print max(number) 
+    number = int(input("Enter a number:"))
+    if number > largest:
+        largest = number
+    if number == -1:
+        print("The largest number you've entered is {0}".format(largest))
+    
+    
+
--- example.py
+++ example.py
@@ -2,7 +2,7 @@ number = int(input("Please enter a number: "))
 high = max(number)
 low = min(number)
 while number == -1:
-    number = int(input("Please enter a number: "))
+    number = int(input("Enter a number: "))
 print ("The highest number entered was {0}".format(high))
 print ("The lowest number entered was {0}".format(low))
 
