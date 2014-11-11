25f863aebcd2a80b44c5e4ec325fb13630d1bd61
Laptop <Laptop@Laptop-PC>
2014-11-11 18:05:03 +0000

    complete

--- GUESS THE NUMBER GAME.py
+++ GUESS THE NUMBER GAME.py
@@ -4,21 +4,20 @@
 import random
 random_number = random.randint(1,100)
 number = 0
-count = 0
 number_of_guesses = 0
 print(random_number)
 while not number == random_number:
-    number_of_guesses = number_of_guesses + count
     number = int(input("Enter a number between 1 and 100:"))
     if number < 0:
-        print("the number is out of range. Enter a number between 1 and 100:")
+        print("The number is out of range. Enter a number between 1 and 100:")
     elif number > 100:
-        print("the number is out of range. Enter a number between 1 and 100:")
+        print("The number is out of range. Enter a number between 1 and 100:")
     elif random_number < number:
-        print("too high")
+        print("Too high")
     elif number < random_number:
-        print("too low")
-print(number_of_guesses)
+        print("Too low")
+    number_of_guesses = number_of_guesses + 1
+print("This is how many guesses you've had {0}.".format(number_of_guesses))
     
         
     
